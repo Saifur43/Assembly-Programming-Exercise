e5f39bd344c2bf6fbfd58a42db01fca7ff172903
Saifur <saifurrahmany43@gmail.com>
2018-03-19 22:13:03 +0600

    adding 4.9

---
+++ Chapter 4/9.asm
@@ -0,0 +1,73 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG DB 'ENTER THREE INITIALS: $'
+.CODE
+MAIN PROC
+    ;INITIALIZE DATA SEGMENT
+    MOV AX,@DATA
+    MOV DS,AX
+    ;PRINT FIRST DATA
+    LEA DX,MSG
+    MOV AH,9
+    INT 21H
+    
+    ;FIRST INPUT
+    MOV AH,1
+    INT 21H
+    MOV BL,AL
+    
+    ;SECOND INPUT
+    MOV AH,1
+    INT 21H
+    MOV CL,AL
+    
+    ;THIRED INPUT
+    MOV AH,1
+    INT 21H
+    MOV BH,AL
+    
+    ;NEW LINE
+    MOV AH,2
+    MOV DL,0DH ;CARRIGE RETURN
+    INT 21H
+    MOV DL,0AH ;NEW LINE
+    INT 21H
+    
+    ;FIRST OUTPUT
+    MOV AH,2
+    MOV DL,BL
+    INT 21H
+            
+    ;NEW LINE
+    MOV DL,0DH ;CARRIGE RETURN
+    INT 21H
+    MOV DL,0AH ;NEW LINE
+    INT 21H
+    
+    ;SECOND OUTPUT
+    MOV AH,2
+    MOV DL,CL
+    INT 21H
+            
+    ;NEW LINE
+    MOV DL,0DH ;CARRIGE RETURN
+    INT 21H
+    MOV DL,0AH ;NEW LINE
+    INT 21H
+    
+    ;THIRD OUTPUT
+    MOV AH,2
+    MOV DL,BH
+    INT 21H
+    
+    ;EXIT TO DOS
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
+    
+    
+    
+
